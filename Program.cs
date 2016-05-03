53f8716cff00657441a15f1eca9dd3a26c8da6aa
GitHubNameNew <iwanovivaylo@gmail.com>
2016-05-03 23:33:48 +0300

    Added files via upload
    Maximal sequence
    Description
    
    Write a program that finds the length of the maximal sequence of equal elements in an array of N integers.

---
+++ Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MaxSequence
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            int[] arr = new int[n];
+            for (int i = 0; i < n; i++)
+            {
+                arr[i] = int.Parse(Console.ReadLine());
+            }
+            int maxSeqSize = 1;
+            int tempSeqSize = 1;
+            int seqElemValue = 1;
+            for (int i = 0; i < arr.Length - 1; i++)
+            {
+                if (arr[i] == arr[i + 1])
+                {
+                    tempSeqSize++;
+
+                    if (tempSeqSize > maxSeqSize)
+                    {
+                        maxSeqSize = tempSeqSize;
+                        arr[i] = seqElemValue;
+                    }
+                }
+                else
+                {
+                    tempSeqSize = 1;
+                }
+            }
+            Console.WriteLine(maxSeqSize);
+        }
+    }
+}
